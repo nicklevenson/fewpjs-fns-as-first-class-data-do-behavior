ccd72615f833ab70e61b896ba657c9cff379faaf
Nicholas Levenson <nicklevenson@gmail.com>
2021-01-26 10:23:07 -0800

    Done.

--- index.js
+++ index.js
@@ -10,4 +10,21 @@ function handleClick(e) {
 /* End Given Code, don't edit above here...*/
 
 /* Write your implementation of greet() */
+function greet(time) {
+  const hours = parseInt(time.split(":")[0])
+  const minutes = parseInt(time.split(":")[1])
+  if(hours < 12){
+    return "Good Morning"
+  }
+  else if(hours >= 12 && hours < 17){
+    return "Good Afternoon"
+  }
+  else {
+    return "Good Evening"
+  }
+}
 /* Write your implementation of displayMessage() */
+function displayMessage(string) {
+  const heading = document.getElementById("greeting") 
+  heading.innerHTML = `${string}`
+}
